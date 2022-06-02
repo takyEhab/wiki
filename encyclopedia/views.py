b919befb464f66b19a1768a593e55351ba467bb5
taky <tkyehab@gmail.com>
2022-06-02 13:48:01 +0200

    better design

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,3 +1,4 @@
+from logging import PlaceHolder
 from django import forms
 from django.forms.widgets import Textarea
 from django.shortcuts import render
@@ -10,8 +11,8 @@ from . import util
 
 
 class CreateNewEntry(forms.Form):
-    TitleEntry = forms.CharField()
-    Entry = forms.CharField(widget=forms.Textarea)
+    TitleEntry = forms.CharField(widget=forms.TextInput(attrs={'class': 'block p-2 text-gray-900 bg-gray-50 rounded-lg border border-gray-300 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500', "PlaceHolder":'Write Title Here ...'}))
+    Entry = forms.CharField(widget=forms.Textarea(attrs={'class': 'block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500', "PlaceHolder":'write Content Here...'}))
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
